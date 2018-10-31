a5fea0ae54864eecbe3e6d60c0af2870cddde3f8
Jose Alberto  Soler <josealbertosoler@Joses-MacBook-Pro.local>
2018-10-31 21:16:27 +0100

    refactors code

--- stitch.py
+++ stitch.py
@@ -3,8 +3,34 @@ import os
 import subprocess
 import argparse
 
+def stitch_gpfusion_images(fusion_path,files_path,output_path,parallax_compensation):
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    back_folder = files_path+'/back/DCIM'
+    subdirs = [x[0] for x in os.walk(back_folder)] 
+    subdirs.pop(0)
+
+    for directory in subdirs:
+        images = [x for x in os.walk(directory)] 
+        images=images[0][2]
+        back_folder_name = os.path.basename(directory)
+        output_directory = output_path+'/'+back_folder_name.replace('BACK','')
+        if not os.path.exists(output_directory):
+            os.mkdir(output_directory)
+        front_folder_name = back_folder_name.replace('BACK','FRNT')
+        front_folder_path = os.path.dirname(directory)+'/'+front_folder_name
+        front_folder_path = front_folder_path.replace('/gopro_fusion/back/DCIM/','/gopro_fusion/front/DCIM/')
+
+        for image in images:
+            back_image = directory+'/'+image
+            front_image = front_folder_path+'/'+os.path.basename(back_image).replace('GB','GF')
+            output_file = os.path.basename(back_image).replace('GB','Stitched')
+
+            command_mac = './FusionStudio --front '+front_image+' --back '+back_image + ' --output '+ output_directory+'/'+output_file + ' --parallaxCompensation '+ str(parallax_compensation)
+            command = command_mac #TODO: command is different in windows environment
+            result = subprocess.run(command,shell=True,cwd=fusion_path)
+
 if __name__ == '__main__':
-    
     parser = argparse.ArgumentParser(description='Stitches Fusion VR images to upload them to Mapillary')
     parser.add_argument('--fusion_path',dest='fusion_path',help='path to desktop Fusion software')
     parser.add_argument('--files_path',dest='files_path',help='root directory of the images (folder above front and back folders)')
@@ -14,32 +40,6 @@ if __name__ == '__main__':
     1 Discrete+Continuous
     2 Discrete''',
     default=0)
-
     args = parser.parse_args()
-    if not os.path.exists(args.output_path):
-        os.mkdir(args.output_path)
-    back_folder = args.files_path+'/back/DCIM'
-    subdirs = [x[0] for x in os.walk(back_folder)] 
-    subdirs.pop(0)
 
-    for directory in subdirs:
-        images = [x for x in os.walk(directory)] 
-        images=images[0][2]
-        back_folder_name = os.path.basename(directory)
-        output_directory = args.output_path+'/'+back_folder_name.replace('BACK','')
-        if not os.path.exists(output_directory):
-            os.mkdir(output_directory)
-        for image in images:
-            back_image = directory+'/'+image
-            front_folder_name = back_folder_name.replace('BACK','FRNT')
-            front_folder_path = os.path.dirname(directory)+'/'+front_folder_name
-            front_folder_path = front_folder_path.replace('/gopro_fusion/back/DCIM/','/gopro_fusion/front/DCIM/')
-            front_image = front_folder_path+'/'+os.path.basename(back_image).replace('GB','GF')
-            output_file = os.path.basename(back_image).replace('GB','Stitched')
-
-            command_mac = './FusionStudio --front '+front_image+' --back '+back_image + ' --output '+ output_directory+'/'+output_file + ' --parallaxCompensation '+ str(args.parallax_compensation)
-            command = command_mac
-            print(command_mac)
-            result = subprocess.run(command,shell=True,cwd=args.fusion_path)
-
-    
+    stitch_gpfusion_images(args.fusion_path,args.files_path,args.output_path,args.parallax_compensation)
